382163c8709518723ef186acc481ef9ddce1e3c4
Samuels5 <samimule20@gmail.com>
2024-07-30 19:34:59 +0300

    adding

---
+++ script.js
@@ -0,0 +1,24 @@
+var input = document.getElementById('input');
+var butt = document.getElementById('button');
+var ol = document.getElementById('ol');
+var add = function () {
+    var li = document.createElement('li');
+    li.textContent = input.value;
+    var editButton = document.createElement('button');
+    editButton.textContent = 'edit';
+    editButton.onclick = function () {
+        var val = prompt('edit it ', li === null || li === void 0 ? void 0 : li.textContent);
+        li.textContent = val;
+    };
+    var deletButton = document.createElement('button');
+    deletButton.textContent = 'delete';
+    deletButton.onclick = function () { var _a, _b, _c; (_c = (_b = (_a = deletButton === null || deletButton === void 0 ? void 0 : deletButton.parentNode) === null || _a === void 0 ? void 0 : _a.parentNode) === null || _b === void 0 ? void 0 : _b.parentNode) === null || _c === void 0 ? void 0 : _c.removeChild(div2); };
+    var div1 = document.createElement('div');
+    var div2 = document.createElement('div');
+    div1.appendChild(editButton);
+    div1.appendChild(deletButton);
+    div2.appendChild(li);
+    div2.appendChild(div1);
+    ol === null || ol === void 0 ? void 0 : ol.appendChild(div2);
+    input.value = '';
+};
---
+++ script.ts
@@ -0,0 +1,30 @@
+
+
+let input = <HTMLInputElement>document.getElementById('input')
+let butt = document.getElementById('button')
+let ol = document.getElementById('ol')
+
+
+const add = () => {
+    let li = document.createElement('li');
+    li.textContent = input.value;
+    let editButton = document.createElement('button');
+    editButton.textContent = 'edit';
+    editButton.onclick = () => {
+        let val = prompt('edit it ', li?.textContent!);
+        li.textContent = val;
+    };
+
+    let deletButton = document.createElement('button');
+    deletButton.textContent = 'delete';
+    deletButton.onclick = function() {deletButton?.parentNode?.parentNode?.parentNode?.removeChild(div2)};
+    let div1 = document.createElement('div');
+    let div2 = document.createElement('div');
+
+    div1.appendChild(editButton);
+    div1.appendChild(deletButton);
+    div2.appendChild(li);
+    div2.appendChild(div1);
+    ol?.appendChild(div2);
+    input.value = '';
+}
